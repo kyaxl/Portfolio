32fbb7d263ae8a912946340b25503ded3eb27b65
Yalmaz Khan <kyalmax49@gmail.com>
2025-05-19 19:26:46 +0500

    Cleaned up dublicate code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Smooth scroll for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(link => {
     link.addEventListener('click', function (e) {
@@ -14,20 +13,3 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     document.querySelector("nav").classList.toggle("dark-mode");
   });
   
-=======
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
-  
-  // Dark mode toggle
-  document.getElementById("theme-toggle").addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    document.querySelector("nav").classList.toggle("dark-mode");
-  });
-  
->>>>>>> 495e223e796c51502b272421727c1ad4cac5df0b
